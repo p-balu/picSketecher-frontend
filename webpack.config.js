ce60c6c70352d74849c80af4475b9ce90a097fd2
Balachanderrao <balachander@betalectic.com>
2022-02-14 23:02:04 +0530

    initial-setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html",
+});
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: {
+          loader: "url-loader",
+        },
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+};
