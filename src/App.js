2d2dc060f0428221d9bf088f7cdfc40666f4045c
Balachanderrao <balachander@betalectic.com>
2022-02-14 23:04:42 +0530

    initial-setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./assets/stylesheets/App.css";
+import Header from "./console/Header";
+import LandingPage from "./console/LandingPage";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <LandingPage />
+    </>
+  );
+};
+
+export default App;
